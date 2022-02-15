86747c803eeadabcff3245b4b6267449d94182fc
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-02-16 07:11:25 +0900

    111

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import "./styles.css";
+import { UserCard } from "./components/UserCard";
+
+const user = {
+  id: 1,
+  name: "じゃけえ",
+  email: "123@aaa.com",
+  address: "suita"
+};
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <UserCard user={user} />
+    </div>
+  );
 }
